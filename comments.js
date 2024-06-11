b1dc7a634bec901703b6aca2d05e0c77dc266702
Wolftex23 <51463234+safarancho26@users.noreply.github.com>
2024-06-11 16:16:20 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+    // create web server
+    var express = require('express');
+    var app = express();
+
+    // create a route
+    app.get('/comments', function(req, res) {
+        res.send('This is the comments page');
+    });
+
+    // start the server
+    app.listen(3000, function() {
+        console.log('Server is running on http://localhost:3000');
+    });
