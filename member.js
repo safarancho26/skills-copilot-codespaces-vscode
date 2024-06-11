5a61939ba9792f9dda5a096ce73ee41420e767e6
Wolftex23 <51463234+safarancho26@users.noreply.github.com>
2024-06-11 16:12:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = [];
+    var skill = {
+        name: "JavaScript",
+        level: "Intermediate"
+    };
+    skills.push(skill);
+    return skills;
+}
